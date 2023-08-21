b96cacaf0bab1fc62f36955dc21221fec62e24db
Theodore Zhang <70734380+Tedinator27@users.noreply.github.com>
2023-08-20 17:30:06 -0700

    Add files via upload

---
+++ timedMathChallenge.py
@@ -0,0 +1,40 @@
+#This program is to generate 10 random math problems for the user to solve.
+import random
+import time
+
+OPERATORS = ["+", "-", "*"] #operators to choose from when generating the problem
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
+
+
+def generate_problem(): #generates the problem
+    left = random.randint(MIN_OPERAND, MAX_OPERAND) #random number between 3 and 12
+    right = random.randint(MIN_OPERAND, MAX_OPERAND) #random number between 3 and 12
+    operator = random.choice(OPERATORS) #random operator
+    #random.choice is going to randomly pick an element from a list
+
+    expr = str(left) + " " + operator + " " + str(right)
+    answer = eval(expr) #eval evalutes a string as if it was a python expression
+    return expr, answer
+
+
+wrong = 0
+input("Press enter to start!")
+print("----------------------")
+
+start_time = time.time()
+
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+        guess = input("Problem #" + str(i + 1) + ": " + expr + " = ")
+        if guess == str(answer):
+            break
+        wrong += 1
+
+end_time = time.time()
+total_time = round(end_time - start_time, 2)
+
+print("----------------------")
+print("Nice work! You finished in", total_time, "seconds!")
