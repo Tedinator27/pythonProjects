72b5679f0aaf0c845939f2201ffb3639771ba175
Theodore Zhang <70734380+Tedinator27@users.noreply.github.com>
2023-12-12 19:49:06 -0800

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,4 +1,5 @@
 #credits to CDcodes on YT
+#this file is the helper file for  ticTacToe
 def draw_board(spots):
     board = (f"|{spots[1]}|{spots[2]}|{spots[3]}|\n"
              f"|{spots[4]}|{spots[5]}|{spots[6]}|\n"
